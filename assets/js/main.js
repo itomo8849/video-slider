2cd550bcf078d7e685f5ee31bd496d6bccc67029
itomo8849 <tomolllllllo@gmail.com>
2025-01-24 17:40:19 +0900

    スクリプトの記述

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+// 動画再生制御の汎用関数
+function controlVideos(swiper) {
+  // すべての動画を停止
+  swiper.el.querySelectorAll('video').forEach((video) => {
+    video.pause();
+    video.currentTime = 0;
+  });
+
+  // アクティブスライド内の動画を再生
+  const activeSlide = swiper.slides[swiper.activeIndex];
+  const videos = activeSlide.querySelectorAll('video');
+  videos.forEach((video) => video.play());
+}
+
+// 動画が1つのみのスライダー
+const singleVideoSlider = new Swiper('.js_singleVideoSlider', {
+  loop: true,
+  autoplay: {
+    delay: 8000,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+singleVideoSlider.on('slideChange', () => controlVideos(singleVideoSlider));
+controlVideos(singleVideoSlider); // 初期表示時の動画再生
+
+// 動画が2つのスライダー
+const multipleVideoSlider = new Swiper('.js_multipleVideoSlider', {
+  loop: true,
+  autoplay: {
+    delay: 8000,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+multipleVideoSlider.on('slideChange', () => controlVideos(multipleVideoSlider));
+controlVideos(multipleVideoSlider); // 初期表示時の動画再生
